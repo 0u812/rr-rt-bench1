9f1a35e9c54b352a92794b141161b89af9f4bc5b
0u812 <0u812@users.noreply.github.com>
2017-06-04 13:31:55 -0700

    add copasi bench

--- rr-rt-bench1.py
+++ rr-rt-bench1.py
@@ -4,12 +4,18 @@ from timeit import default_timer
 
 r = RoadRunner('WolfGlycolysis.xml')
 
-orders = 5
-for multiplier in np.logspace(0,orders,orders+1):
-    #print('multiplier = {}'.format(multiplier))
-    t1 = default_timer()
-    r.reset()
-    r.simulate(0,2*int(multiplier),1000*int(multiplier))
-    t2 = default_timer()
-    print('{} simulation, duration = {}'.format(2*int(multiplier), t2-t1))
-    #r.plot()
+#orders = 5
+#for multiplier in np.logspace(0,orders,orders+1):
+    ##print('multiplier = {}'.format(multiplier))
+    #t1 = default_timer()
+    #r.reset()
+    #r.simulate(0,2*int(multiplier),1000*int(multiplier))
+    #t2 = default_timer()
+    #print('{} simulation, duration = {}'.format(2*int(multiplier), t2-t1))
+    ##r.plot()
+
+# COPASI 4:20
+t1 = default_timer()
+r.reset()
+r.simulate(0,20000,200000)
+t2 = default_timer()
